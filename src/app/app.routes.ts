979a29af347e503aa5200696abff02dc80232ed9
Johnbull-eghianruwa <wisdomuwa005@gmail.com>
2026-06-21 18:07:44 +0200

    Add application shell with main layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MainLayout } from './core/layouts/main-layout/main-layout';
+
+import { Home } from './features/home/home';
+import { Login } from './features/auth/login/login';
+import { Signup } from './features/auth/signup/signup';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+            {
+                path: '',
+                component: Home,
+                pathMatch: 'full'
+            },
+            {
+                path: 'login',
+                component: Login
+            },
+            {
+                path: 'signup',
+                component: Signup
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
--- src/app/features/auth/signup/signup.spec.ts
+++ src/app/features/auth/signup/signup.spec.ts
@@ -3,20 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Signup } from './signup';
 
 describe('Signup', () => {
-  let component: Signup;
-  let fixture: ComponentFixture<Signup>;
+    let component: Signup;
+    let fixture: ComponentFixture<Signup>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Signup],
-    }).compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [Signup],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(Signup);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+        fixture = TestBed.createComponent(Signup);
+        component = fixture.componentInstance;
+        await fixture.whenStable();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
